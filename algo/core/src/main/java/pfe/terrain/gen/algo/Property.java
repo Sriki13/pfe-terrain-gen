9031583ac47568e03ab2efdf6beee6bbaf816a41
JA <jean-adam.puskaric@etu.unice.fr>
2018-10-27 19:16:20 +0200

    Forgot to clean some stuff

--- algo/core/src/main/java/pfe/terrain/gen/algo/Property.java
+++ algo/core/src/main/java/pfe/terrain/gen/algo/Property.java
@@ -1,30 +1,16 @@
 package pfe.terrain.gen.algo;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import pfe.terrain.gen.algo.geometry.Edge;
-
-import java.util.Set;
-
 public enum Property {
-    POINTS("Points", Set.class, Coordinate.class), VERTICES("Vertices", Set.class, Coordinate.class), EDGES("Edges", Set.class, Edge.class), FACES("Faces", Set.class, Edge.class);
+    POINTS("Points"), VERTICES("Vertices"), EDGES("Edges"), FACES("Faces");
 
-    private final Class<?> type;
     private final String name;
-    private final Class<?> subType;
 
-    Property(String name, Class<?> type, Class<?> subtype) {
+    Property(String name) {
         this.name = name;
-        this.type = type;
-        this.subType = subtype;
     }
 
     public String getName() {
         return name;
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> Class<T> getType() {
-        return (Class<T>) type;
     }
 
 }
