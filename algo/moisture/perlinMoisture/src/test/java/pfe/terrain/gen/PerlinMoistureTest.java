60643899a9155d8f041952fa27088619bbc6bc24
JA <jean-adam.puskaric@etu.unice.fr>
2018-11-19 17:41:48 +0100

    #34 small update to frequency

--- algo/moisture/perlinMoisture/src/test/java/pfe/terrain/gen/PerlinMoistureTest.java
+++ algo/moisture/perlinMoisture/src/test/java/pfe/terrain/gen/PerlinMoistureTest.java
@@ -1,8 +1,6 @@
 package pfe.terrain.gen;
 
-import com.flowpowered.noise.module.Module;
 import com.flowpowered.noise.module.source.Perlin;
-import com.flowpowered.noise.module.source.RidgedMulti;
 import com.vividsolutions.jts.geom.Coordinate;
 import org.junit.Test;
 import pfe.terrain.gen.algo.geometry.Face;
@@ -24,21 +22,22 @@ public class PerlinMoistureTest {
                 faces.add(new Face(new Coordinate(i, j), new ArrayList<>()));
             }
         }
-        Perlin perlin = new Perlin();
 //        for (Face face : faces) {
 //            Coordinate c = face.getCenter();
 //            System.out.println(c);
 //            System.out.println(perlin.getValue(c.x, c.y, 0));
 //        }
-        final int width = 1024, height = 1024;
-        final Module module = new RidgedMulti();
-        ((RidgedMulti) module).setSeed(new Random().nextInt());
-        final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_USHORT_GRAY);
+        final int width = 64;
+        final Perlin perlin = new Perlin();
+        perlin.setSeed(1);
+        perlin.setFrequency(10.0);
+
+        final BufferedImage image = new BufferedImage(width, width, BufferedImage.TYPE_USHORT_GRAY);
         short[] data = ((DataBufferUShort) image.getRaster().getDataBuffer()).getData();
-        List<Double> values = new ArrayList<>(width * height);
-        for (int y = 0; y < height; y++) {
+        List<Double> values = new ArrayList<>(width * width);
+        for (int y = 0; y < width; y++) {
             for (int x = 0; x < width; x++) {
-                final double noise = module.getValue(x / (float) width, y / (float) height, 0);
+                final double noise = perlin.getValue(x / (float) width, y / (float) width, 0);
                 //System.out.println(noise);
                 values.add(y * width + x, noise);
                 //data[y * width + x] = noise;
@@ -47,7 +46,7 @@ public class PerlinMoistureTest {
         double max = Collections.max(values);
         double min = Collections.min(values);
         values.replaceAll(val -> (val - min) / (max - min));
-        System.out.println(values);
+        //System.out.println(values);
         for (int i = 0; i < values.size(); i++) {
             data[i] = (short) Math.round(values.get(i) * 65536);
         }
