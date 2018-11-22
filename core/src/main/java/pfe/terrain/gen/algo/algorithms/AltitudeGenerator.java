a8d78f401521d8ce6d061d804fd16a72f82ff03a
Guillaume André <guillaume.andre@etu.unice.fr>
2018-11-22 09:35:14 +0100

    Update contracts #17

--- core/src/main/java/pfe/terrain/gen/algo/algorithms/AltitudeGenerator.java
+++ core/src/main/java/pfe/terrain/gen/algo/algorithms/AltitudeGenerator.java
@@ -2,25 +2,23 @@ package pfe.terrain.gen.algo.algorithms;
 
 import pfe.terrain.gen.algo.Key;
 import pfe.terrain.gen.algo.SerializableKey;
-import pfe.terrain.gen.algo.WaterKind;
 import pfe.terrain.gen.algo.constraints.Constraints;
 import pfe.terrain.gen.algo.constraints.Contract;
 import pfe.terrain.gen.algo.types.BooleanType;
+import pfe.terrain.gen.algo.types.DoubleType;
 
-public abstract class WaterGenerator extends Contract {
+public abstract class AltitudeGenerator extends Contract {
 
     protected final Key<BooleanType> verticeBorderKey = new Key<>(verticesPrefix + "IS_BORDER", BooleanType.class);
     protected final Key<BooleanType> faceBorderKey = new Key<>(facesPrefix + "IS_BORDER", BooleanType.class);
 
-    protected Key<BooleanType> faceWaterKey = new SerializableKey<>(facesPrefix + "IS_WATER", "isWater", BooleanType.class);
-    protected Key<BooleanType> vertexWaterKey = new SerializableKey<>(verticesPrefix + "IS_WATER", "isWater", BooleanType.class);
-    protected Key<WaterKind> waterKindKey = new SerializableKey<>(facesPrefix + "WATER_KIND", "waterKind", WaterKind.class);
+    protected Key<DoubleType> vertexHeightKey = new SerializableKey<>(verticesPrefix + "HEIGHT", "height", DoubleType.class);
 
     @Override
     public Constraints getContract() {
         return new Constraints(
                 asSet(faces, vertices, verticeBorderKey, faceBorderKey),
-                asSet(faceWaterKey, vertexWaterKey, waterKindKey)
+                asSet(vertexHeightKey)
         );
     }
 
