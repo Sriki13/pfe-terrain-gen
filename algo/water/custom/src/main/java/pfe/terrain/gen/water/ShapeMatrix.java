e5ca25ab19b6ed38c348d6d0f0455e4f05459f07
JA <jean-adam.puskaric@etu.unice.fr>
2018-11-27 14:16:39 +0100

    #58 created new algorithm for custom shapes

---
+++ algo/water/custom/src/main/java/pfe/terrain/gen/water/ShapeMatrix.java
@@ -0,0 +1,26 @@
+package pfe.terrain.gen.water;
+
+public class ShapeMatrix {
+
+    private boolean[][] matrix;
+    private final int size;
+
+    public ShapeMatrix(String param) throws Exception {
+        String lines[] = param.split("\\r?\\n");
+        size = lines.length;
+        matrix = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < lines[i].length(); j++) {
+                matrix[i][j] = (lines[i].charAt(j) == '1');
+            }
+        }
+    }
+
+    public boolean isWater(int x, int y) {
+        return !(matrix[x][y]);
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
