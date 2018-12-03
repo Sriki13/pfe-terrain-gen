4c56e7b108e4ef544e65ee0b58b209616e79dcec
JA <jean-adam.puskaric@etu.unice.fr>
2018-12-03 14:08:44 +0100

    #90 Starting pitch implementation

---
+++ algo/pitch/basic/src/main/java/pfe/terrain/gen/algo/borders/BasicPitch.java
@@ -0,0 +1,54 @@
+package pfe.terrain.gen.algo.borders;
+
+import pfe.terrain.gen.algo.constraints.Constraints;
+import pfe.terrain.gen.algo.constraints.Contract;
+import pfe.terrain.gen.algo.context.Context;
+import pfe.terrain.gen.algo.geometry.Coord;
+import pfe.terrain.gen.algo.geometry.Face;
+import pfe.terrain.gen.algo.geometry.FaceSet;
+import pfe.terrain.gen.algo.island.IslandMap;
+import pfe.terrain.gen.algo.key.Key;
+import pfe.terrain.gen.algo.key.SerializableKey;
+import pfe.terrain.gen.algo.types.DoubleType;
+
+import java.util.Comparator;
+import java.util.Set;
+
+public class BasicPitch extends Contract {
+
+    public static final SerializableKey<DoubleType> facePitchKey =
+            new SerializableKey<>(facesPrefix + "HAS_PITCH", "pitch", DoubleType.class);
+
+    public static final Key<DoubleType> vertexHeightKey =
+            new SerializableKey<>(verticesPrefix + "HEIGHT", "height", DoubleType.class);
+
+    @Override
+    public Constraints getContract() {
+        return new Constraints(
+                asKeySet(faces, vertexHeightKey),
+                asKeySet(facePitchKey)
+        );
+    }
+
+
+    private final static Comparator<Coord> byHeight =
+            (o1, o2) ->
+                    (int) (1000 * (o2.getProperty(vertexHeightKey).value - o1.getProperty(vertexHeightKey).value));
+
+    @Override
+    public void execute(IslandMap islandMap, Context context) {
+        FaceSet faces = islandMap.getFaces();
+        for (Face face : faces) {
+            face.putProperty(facePitchKey, new DoubleType(computePitch(face)));
+        }
+    }
+
+    double computePitch(Face face) {
+        Set<Coord> borders = face.getBorderVertices();
+        Coord lowest = borders.stream().min(byHeight).get();
+        Coord highest = borders.stream().max(byHeight).get();
+        double rise = highest.getProperty(vertexHeightKey).value - lowest.getProperty(vertexHeightKey).value;
+        double run = Math.sqrt(Math.pow(highest.x - lowest.x, 2) + Math.pow(highest.y - lowest.y, 2));
+        return 100 * (rise / run);
+    }
+}
