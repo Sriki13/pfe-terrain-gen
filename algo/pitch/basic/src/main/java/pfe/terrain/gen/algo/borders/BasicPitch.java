a13980206c74c796292353074450ad2af8738ced
JA <jean-adam.puskaric@etu.unice.fr>
2018-12-03 14:46:26 +0100

    #90 Pitch finished

--- algo/pitch/basic/src/main/java/pfe/terrain/gen/algo/borders/BasicPitch.java
+++ algo/pitch/basic/src/main/java/pfe/terrain/gen/algo/borders/BasicPitch.java
@@ -16,10 +16,10 @@ import java.util.Set;
 
 public class BasicPitch extends Contract {
 
-    public static final SerializableKey<DoubleType> facePitchKey =
+    static final SerializableKey<DoubleType> facePitchKey =
             new SerializableKey<>(facesPrefix + "HAS_PITCH", "pitch", DoubleType.class);
 
-    public static final Key<DoubleType> vertexHeightKey =
+    static final Key<DoubleType> vertexHeightKey =
             new SerializableKey<>(verticesPrefix + "HEIGHT", "height", DoubleType.class);
 
     @Override
@@ -33,22 +33,24 @@ public class BasicPitch extends Contract {
 
     private final static Comparator<Coord> byHeight =
             (o1, o2) ->
-                    (int) (1000 * (o2.getProperty(vertexHeightKey).value - o1.getProperty(vertexHeightKey).value));
+                    (int) (1000 * (o1.getProperty(vertexHeightKey).value - o2.getProperty(vertexHeightKey).value));
 
     @Override
     public void execute(IslandMap islandMap, Context context) {
         FaceSet faces = islandMap.getFaces();
+        double factor = islandMap.getSize() / 1600.0;
         for (Face face : faces) {
-            face.putProperty(facePitchKey, new DoubleType(computePitch(face)));
+            face.putProperty(facePitchKey, new DoubleType(computePitch(face, factor)));
         }
     }
 
-    double computePitch(Face face) {
+    double computePitch(Face face, double factor) {
         Set<Coord> borders = face.getBorderVertices();
         Coord lowest = borders.stream().min(byHeight).get();
         Coord highest = borders.stream().max(byHeight).get();
         double rise = highest.getProperty(vertexHeightKey).value - lowest.getProperty(vertexHeightKey).value;
-        double run = Math.sqrt(Math.pow(highest.x - lowest.x, 2) + Math.pow(highest.y - lowest.y, 2));
+        double run = Math.sqrt(Math.pow(highest.x - lowest.x, 2) + Math.pow(highest.y - lowest.y, 2)) * factor;
+        if (run == 0) return 0;
         return 100 * (rise / run);
     }
 }
