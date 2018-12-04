c1355fd1abd732de31ce97f0cbc5b21023d77a59
Lucas Matteo <lucas.matteo@etu.unice.fr>
2018-12-04 10:38:46 +0100

    travisfix

--- travisScripts/buildAndPush.sh
+++ travisScripts/buildAndPush.sh
@@ -2,7 +2,7 @@
 
 
 cd ../generator-factory
-docker build -t $DOCKER_USERNAME/PfeGeneratorFactory .
+docker build -t $DOCKER_USERNAME/pfe_generator_factory .
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_USERNAME/PfeGeneratorFactory
+docker push $DOCKER_USERNAME/pfe_generator_factory
