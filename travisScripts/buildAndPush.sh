cc9f508dffaa62c9fcf1e0b27a267767255678f2
Lucas Matteo <lucas.matteo@etu.unice.fr>
2018-12-04 10:28:27 +0100

    build file for travis #72

---
+++ travisScripts/buildAndPush.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+cd ../generator-factory
+docker build -t $DOCKER_USERNAME/PfeGeneratorFactory .
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push $DOCKER_USERNAME/PfeGeneratorFactory
