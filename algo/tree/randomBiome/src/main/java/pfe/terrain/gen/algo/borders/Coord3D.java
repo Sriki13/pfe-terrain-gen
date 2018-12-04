bdb36a5ffec81408e2fdc20dbc743dcd381e9437
JA <jean-adam.puskaric@etu.unice.fr>
2018-12-04 16:28:28 +0100

    #91 Added parameters to tree gen

---
+++ algo/tree/randomBiome/src/main/java/pfe/terrain/gen/algo/borders/Coord3D.java
@@ -0,0 +1,13 @@
+package pfe.terrain.gen.algo.borders;
+
+public class Coord3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public Coord3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
--- algo/tree/randomBiome/src/main/java/pfe/terrain/gen/algo/borders/RandomTreesByBiome.java
+++ algo/tree/randomBiome/src/main/java/pfe/terrain/gen/algo/borders/RandomTreesByBiome.java
@@ -4,6 +4,7 @@ import pfe.terrain.gen.algo.constraints.Constraints;
 import pfe.terrain.gen.algo.constraints.Contract;
 import pfe.terrain.gen.algo.constraints.context.Context;
 import pfe.terrain.gen.algo.constraints.key.Key;
+import pfe.terrain.gen.algo.constraints.key.Param;
 import pfe.terrain.gen.algo.constraints.key.SerializableKey;
 import pfe.terrain.gen.algo.island.Biome;
 import pfe.terrain.gen.algo.island.IslandMap;
@@ -15,20 +16,31 @@ import pfe.terrain.gen.algo.types.DoubleType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class RandomTreesByBiome extends Contract {
 
+    private static final Param<Double> PITCH_IMPORTANCE = Param.generateDefaultDoubleParam("pitchImportance",
+            "How the pitch affects the tree spawn chance : 0=no effect, 1=huge effect", 0.5, "Importance of face pitch");
 
-    static final Key<Biome> FACE_BIOME_KEY =
+    private static final Param<Double> TREE_DENSITY = Param.generateDefaultDoubleParam("treeDensity",
+            "How many trees are generated per face : 0=very few, 1=a lot", 0.5, "Density/Number of trees");
+
+    @Override
+    public Set<Param> getRequestedParameters() {
+        return asParamSet(PITCH_IMPORTANCE, TREE_DENSITY);
+    }
+
+    private static final Key<Biome> FACE_BIOME_KEY =
             new SerializableKey<>(FACES_PREFIX + "BIOME", "biome", Biome.class);
 
-    static final SerializableKey<DoubleType> FACE_PITCH_KEY =
+    private static final SerializableKey<DoubleType> FACE_PITCH_KEY =
             new SerializableKey<>(FACES_PREFIX + "PITCH", "pitch", DoubleType.class);
 
-    static final SerializableKey<TreeType> TREES_KEY =
+    private static final SerializableKey<TreeType> TREES_KEY =
             new SerializableKey<>("TREES", "trees", TreeType.class);
 
-    static final Key<DoubleType> VERTEX_HEIGHT_KEY =
+    private static final Key<DoubleType> VERTEX_HEIGHT_KEY =
             new SerializableKey<>(VERTICES_PREFIX + "HEIGHT", "height", DoubleType.class);
 
 
@@ -43,30 +55,33 @@ public class RandomTreesByBiome extends Contract {
     @Override
     public void execute(IslandMap islandMap, Context context) {
         FaceSet faces = islandMap.getFaces();
-        List<JsonCoord> coords = new ArrayList<>();
+        double treeDensity = 3 + 3 * context.getParamOrDefault(TREE_DENSITY);
+        double pitchImportance = 500 - (320 * context.getParamOrDefault(PITCH_IMPORTANCE));
+        List<Coord3D> coords = new ArrayList<>();
         Random random = new Random(islandMap.getSeed());
         Biome faceBiome;
         double pitch, z1, z2, z3, l1, l2, det;
         for (Face face : faces) {
             faceBiome = face.getProperty(FACE_BIOME_KEY);
             // pitch influence, high pitch means less trees
-            pitch = 1 - (face.getProperty(FACE_PITCH_KEY).value / 300);
+            pitch = 1 - (face.getProperty(FACE_PITCH_KEY).value / pitchImportance);
             if (pitch < 0) {
                 pitch = 0;
             }
+            // Some biome like OCEAN or LAKE are ignored
             if (faceBiome != Biome.OCEAN && faceBiome != Biome.LAKE && faceBiome != Biome.GLACIER) {
                 for (Coord[] triangle : face.getTriangles()) {
                     z1 = triangle[0].getProperty(VERTEX_HEIGHT_KEY).value;
                     z2 = triangle[1].getProperty(VERTEX_HEIGHT_KEY).value;
                     z3 = triangle[2].getProperty(VERTEX_HEIGHT_KEY).value;
                     det = (triangle[1].y - triangle[2].y) * (triangle[0].x - triangle[2].x) + (triangle[2].x - triangle[1].x) * (triangle[0].y - triangle[2].y);
-                    for (int i = 0; i < Math.round(random.nextInt(6) * faceBiome.getTreeDensity() * pitch); i++) {
+                    for (int i = 0; i < Math.round(random.nextInt((int) treeDensity) * faceBiome.getTreeDensity() * pitch); i++) {
                         // Get x and y from a random point inside a triangle
                         Coord c = Face.getRandomPointInsideTriangle(triangle, random);
                         // Linear interpolation from barycentric coordinates to get z
                         l1 = ((triangle[1].y - triangle[2].y) * (c.x - triangle[2].x) + (triangle[2].x - triangle[1].x) * (c.y - triangle[2].y)) / det;
                         l2 = ((triangle[2].y - triangle[0].y) * (c.x - triangle[2].x) + (triangle[0].x - triangle[2].x) * (c.y - triangle[2].y)) / det;
-                        coords.add(new JsonCoord(c.x, c.y, l1 * z1 + l2 * z2 + (1 - l1 - l2) * z3));
+                        coords.add(new Coord3D(c.x, c.y, l1 * z1 + l2 * z2 + (1 - l1 - l2) * z3));
                     }
                 }
             }
