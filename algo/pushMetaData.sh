74542a50a6bb9fdc4c7a892109ebdfcf58c7f3a0
Lucas Matteo <lucas.matteo@etu.unice.fr>
2018-12-06 11:39:56 +0100

    fix pushing

--- algo/pushMetaData.sh
+++ algo/pushMetaData.sh
@@ -5,8 +5,10 @@ do
     cd $module
     for contract in `ls -d */`
     do
+        echo $contract
         cd $contract
         name=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId | grep -v '\['`
+        echo $name
         mvn -q exec:java -Dexec.mainClass="pfe.terrain.gen.algo.reflection.ContractPrinter" > $name.json
         curl -X PUT -T $name.json http://35.189.252.97/artifactory/algo-data/$name.json
         rm $name.json
