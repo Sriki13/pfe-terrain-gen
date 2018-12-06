56c345ea7e1105915af09927739d2242ca9eb44f
Lucas Matteo <lucas.matteo@etu.unice.fr>
2018-12-06 18:36:26 +0100

    fix dceploy

--- algo/pushMetaData.sh
+++ algo/pushMetaData.sh
@@ -6,7 +6,7 @@ do
     for contract in `ls -d */`
     do
         cd $contract
-        name=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId | grep -v '\['`
+        name=`mvn help:evaluate -Dexpression=project.artifactId | grep -v '\['`
         mvn -q exec:java -Dexec.mainClass="pfe.terrain.gen.algo.reflection.ContractPrinter" > $name.json
         curl -X PUT -T $name.json http://35.189.252.97/artifactory/algo-data/$name.json
         rm $name.json
