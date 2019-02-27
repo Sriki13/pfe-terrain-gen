6ca31b4e2c3a376a1107185c65d8c3bd2f39ffe4
Lucas Matteo <lucas.matteo@etu.unice.fr>
2019-02-27 13:09:48 +0100

    update IP

--- algo/pushMetaData.sh
+++ algo/pushMetaData.sh
@@ -9,7 +9,7 @@ do
         cd $contract
         name=`mvn help:evaluate -Dexpression=project.artifactId | grep -v '\['`
         mvn -q exec:java -Dexec.mainClass="pfe.terrain.gen.algo.reflection.ContractPrinter" > $name.json
-        curl -X PUT -T $name.json http://35.189.252.97/artifactory/algo-data/$name.json
+        curl -X PUT -T $name.json http://34.76.98.30/artifactory/algo-data/$name.json
         rm $name.json
         cd ..
     done
